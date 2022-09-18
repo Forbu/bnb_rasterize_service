894566c411a9e4dea673ade4c79ae2e0cc457b13
Adrien B <forbu14@gmail.com>
2022-09-19 00:26:31 +0200

    initialization for raster writing

--- scripts/rasterize_full_country.py
+++ scripts/rasterize_full_country.py
@@ -13,7 +13,7 @@ from shapely.geometry import MultiPoint
 
 import numpy as np
 
-from bnbserver import extracts_functions
+#from bnbserver import extracts_functions
 
 # read france shapefile in data/regions_2015_metropole_region.shp
 data = gpd.read_file("/home/data/regions_2015_metropole_region.json")
@@ -21,8 +21,17 @@ data = gpd.read_file("/home/data/regions_2015_metropole_region.json")
 # change crs to 2154
 data = data.to_crs("EPSG:2154")
 
+# we create a raster file for each 1km2 tile for the whole france (EPSG:2154)
+# first we get the limits of the france shapefile
+xmin, ymin, xmax, ymax = data.total_bounds
+
+print(xmin, ymin, xmax, ymax)
+
 # now for every polygon in the dataframe we wan loop over the 1km2 tiles and create a raster file for each of them
 for index, row in data.iterrows():
+
+    print(index)
+    print(row)
 
     # first we the bound of the polygon
     bbox = row['geometry'].bounds 
@@ -30,21 +39,14 @@ for index, row in data.iterrows():
     xmin, ymin, xmax, ymax = row['geometry'].bounds
     
     # now we create the 1km2 tiles
-    x = np.arange(np.floor(xmin), np.ceil(xmax) + 1)  # array([0., 1., 2.])
-    y = np.arange(np.floor(ymin), np.ceil(ymax) + 1)  # array([0., 1., 2.])
+    x = np.arange(np.floor(xmin), np.ceil(xmax) + 1, step=1000)  # array([0., 1., 2.])
+    y = np.arange(np.floor(ymin), np.ceil(ymax) + 1, step=1000)  # array([0., 1., 2.])
+
     points = MultiPoint(np.transpose([np.tile(x, len(y)), np.repeat(y, len(x))]))
-    
+
     # now we get all the tiles that are inside the polygon
     result = points.intersection(row['geometry'])
 
-    # now we loop over the tiles and create a raster file for each of them
-    for tile in result:
-        
-        array = extracts_functions.get_raster_value(tile.centroid.x, tile.centroid.y, 500, feature="igntop202103_bat_hauteur")
-
-        # now we save the array in a raster file
-        # first we create the directory if it doesn't exist
-        directory = "/home/data/rasterized_tiles/" + row['nom_reg'] + "/" + str(int(tile.centroid.x)) + "_" + str(int(tile.centroid.y)) + "/"
 
 
 
